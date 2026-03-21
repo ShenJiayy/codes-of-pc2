85ecd3760e83ea7520c4bca435da3c5b538cbeb9
Shenjiayy <2696194778@qq.com>
2026-03-21 14:14:07 +0800

    aaaa

--- Luogu/Contest/A/upload.cpp
+++ Luogu/Contest/A/upload.cpp
@@ -3,5 +3,16 @@ using namespace std;
 int main() {
     int m;
     cin >> m;
-    
+    // 构造a_i|a_j
+    // i * (i - 1) / 2 = m;
+    int l = 1, r = m, n;
+    while (l <= r) {
+        int mid = l + r >> 1;
+        // 也就是求m=n*(n-1)/2的最接近的最小值
+        if (mid * (mid - 1) / 2 <= m)
+            n = mid, l = mid + 1;
+        else r = mid - 1;
+    }
+    cout << n;
+    return 0;
 }
