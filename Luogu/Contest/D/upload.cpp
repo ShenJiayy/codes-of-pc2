70e710fc190f8ba6e25bcbb7a6af7b4426bb6499
Shenjiayy <2696194778@qq.com>
2026-03-21 16:06:05 +0800

    commit

--- Luogu/Contest/D/upload.cpp
+++ Luogu/Contest/D/upload.cpp
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N = 1e5;
-int a[N + 5], curr;
-void dfs(int m, int stp) {
-    if (m == 0) return ;
-    int l = 1, r = N + 3, n;
-    while (l <= r) {
-        int mid = l + r >> 1;
-        if (mid * (mid - 1) / 2 <= m)
-            n = mid, l = mid + 1;
-        else r = mid - 1;
-    }
-    int rm = m - n * (n - 1) / 2;
-    dfs(rm, stp + 1);
-    for (int i = 1; i <= n; i ++)
-        a[++ curr] = stp;
-}
+const int N = 5e6, mod = 998244353;
+int a[N + 5];
+bool vis[N + 5];
 signed main() {
-    int m;
-    cin >> m;
-    if (m == 0) {
-        cout << "2\n99 37";
-        return 0;
-    }
-    dfs(m, 1);
-    cout << curr << endl;
-    for (int i = 1; i <= curr; i ++)
-        cout << a[i] << " ";
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= m; i ++) a[i] = i - 1;
+    int sum = 0;
+    do {
+        memset(vis, 0, sizeof vis);
+        for (int i = 1; i <= m; i ++)
+            for (int j = i; j <= m; j ++) {
+                for (int k = i; k <= j; k ++)
+                    vis[a[k]] = 1;
+            }
+        for (int i = 1; i <= m + 1; i ++)
+            sum += vis[i], sum %= mod;
+    } while (next_permutation(a + 1, a + m + 1));
+    cout << sum;
     return 0;
 }
