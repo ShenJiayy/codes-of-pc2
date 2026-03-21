51f0ae7e986fe00a375996374d2e94dbce218edc
Shenjiayy <2696194778@qq.com>
2026-03-21 14:59:30 +0800

    commit

--- Luogu/Contest/A/upload.cpp
+++ Luogu/Contest/A/upload.cpp
@@ -1,32 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-signed main() {
-    int m;
-    cin >> m;
-    // 构造a_i|a_j
-    // i * (i - 1) / 2 = m;
-    if (m == 0) {
-        cout << "2\n99 37";
-        return 0;
-    }
-    // else if (m == 1) {
-    //     cout << "2\n1 2";
-    //     return 0;
-    // }
+const int N = 1e5;
+int a[N + 5], curr;
+void dfs(int m, int stp) {
+    if (m == 0) return ;
     int l = 1, r = 1e5, n;
     while (l <= r) {
         int mid = l + r >> 1;
-        // 也就是求m=n*(n-1)/2的最接近的最小值
         if (mid * (mid - 1) / 2 <= m)
             n = mid, l = mid + 1;
         else r = mid - 1;
     }
     int rm = m - n * (n - 1) / 2;
-    cout << n + rm << endl;
-    for (int i = 1; i <= 2 * rm; i ++)
-        cout << 4 << " ";
-    for (int i = 1; i <= n - rm; i ++)
-        cout << 2 << " ";
+    dfs(rm, stp + 1);
+    for (int i = 1; i <= n; i ++)
+        a[++ curr] = stp;
+}
+signed main() {
+    int m;
+    cin >> m;
+    if (m == 0) {
+        cout << "2\n99 37";
+        return 0;
+    }
+    dfs(m, 1);
+    cout << curr << endl;
+    for (int i = 1; i <= curr; i ++)
+        cout << a[i] << " ";
+    // int l = 1, r = 1e5, n;
+    // while (l <= r) {
+    //     int mid = l + r >> 1;
+    //     if (mid * (mid - 1) / 2 <= m)
+    //         n = mid, l = mid + 1;
+    //     else r = mid - 1;
+    // }
+    // int rm = m - n * (n - 1) / 2;
+    // cout << n + rm << endl;
+    // for (int i = 1; i <= 2 * rm; i ++)
+    //     cout << 2 << " ";
+    // for (int i = 1; i <= n - rm; i ++)
+    //     cout << 1 << " ";
     return 0;
 }
