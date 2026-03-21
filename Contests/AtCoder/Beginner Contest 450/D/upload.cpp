ef5c7b787d1b38d3615ef46f14dba7986ec1d9d1
ShenJiayy <2696194778@qq.com>
2026-03-21 23:03:09 +0800

    commit

--- Contests/AtCoder/Beginner Contest 450/D/upload.cpp
+++ Contests/AtCoder/Beginner Contest 450/D/upload.cpp
@@ -4,15 +4,21 @@ using namespace std;
 const int N = 2e5;
 int a[N + 5];
 signed main() {
-    int n, k;
+    int n, k, ans = 9e18;
     cin >> n >> k;
     for (int i = 1; i <= n; i ++)
         cin >> a[i];
     for (int i = 1; i <= n; i ++)
         a[i] %= k;
+    sort(a + 1, a + n + 1);
+    deque<int> dq;
+    for (int i = 1; i <= n; i ++)
+        dq.push_back(a[i]);
     for (int i = 1; i <= n; i ++) {
-        
+        dq.push_back(dq.front() + k);
+        dq.pop_front();
+        ans = min(ans, dq.back() - dq.front());
     }
-    cout << *max_element(a + 1, a + n + 1) - *min_element(a + 1, a + n + 1);
+    cout << ans;
     return 0;
 }
