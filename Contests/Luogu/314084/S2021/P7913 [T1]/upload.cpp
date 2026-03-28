f1429c50fa06f675cac80b08b23c4b6de1e6d87d
ShenJiayy <2696194778@qq.com>
2026-03-28 16:57:50 +0800

    commit

---
+++ Contests/Luogu/314084/S2021/P7913 [T1]/upload.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int N = 1e5;
+int f1[N + 5], f2[N + 5], tme[N + 5];
+pair<int, int> p[N + 5];
+void solve(int m, int f[]) {
+	for (int i = 1; i <= m; i ++)
+		scanf("%lld %lld", &p[i].first, &p[i].second);
+	sort(p + 1, p + m + 1);
+	int cur = 0;
+	for (int i = 1; i <= m; i ++) {
+		tme[i] = 0;
+		int a = p[i].first, b = p[i].second, pos = 1;
+		while (pos <= cur && tme[pos] > a) pos ++;
+		f[pos] ++, tme[pos] = b;
+		if (pos >= cur) cur = pos;
+	}
+	for (int i = 1; i <= m; i ++)
+		f[i] += f[i - 1];
+}
+signed main() {
+	int n, m1, m2;
+	scanf("%lld %lld %lld", &n, &m1, &m2);
+	solve(m1, f1);
+	solve(m2, f2);
+	int maxn = 0;
+	for (int i = 0; i <= n + 1; i ++)
+		maxn = max(maxn, f1[i] + f2[n + 1 - i]);
+	printf("%lld", maxn);
+	return 0;
+}
