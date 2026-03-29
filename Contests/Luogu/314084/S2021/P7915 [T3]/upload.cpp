f9b2d183c9087e46d75749fc2646dd7036252498
ShenJiayy <2696194778@qq.com>
2026-03-29 18:34:58 +0800

    commit

--- Contests/Luogu/314084/S2021/P7915 [T3]/upload.cpp
+++ Contests/Luogu/314084/S2021/P7915 [T3]/upload.cpp
@@ -2,7 +2,30 @@
 using namespace std;
 #define int long long
 const int N = 5e5;
+int a[N * 2 + 5];
+char ans[N *2 + 5];
+bool dfs(int id, int l, int r, int s, int t) {
 
+}
+void solve() {
+    int n;
+    scanf("%lld", n);
+    int m = 2 * n;
+    for (int i = 1; i <= m; i ++) scanf("%lld", a+i);
+    memset(ans, 0, sizeof ans);
+    ans[m] = 'L';
+    ans[1] = 'L'; int p = 2;
+    while (a[p] != a[1]) p ++;
+    if (dfs(1, 1, m, p, p)) {
+        puts(ans); return ;
+    }
+    ans[1] = 'L', p = 2;
+    while (a[p] != a[1]) p ++;
+    if (dfs(1, 1, m, p, p)) {
+        puts(ans); return ;
+    }
+    puts("-1");
+}
 signed main() {
 	int T;
     cin >> T;
