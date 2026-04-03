1dbdc418b678e6f8e2356ae679fffa30f7c9b0c6
ShenJiayy <2696194778@qq.com>
2026-04-03 21:20:36 +0800

    commit

--- Problems/MX/CF1610D/upload.cpp
+++ Problems/MX/CF1610D/upload.cpp
@@ -5,8 +5,14 @@ const int N = 2e5, mod = 1e9 + 7;
 int n, a[N + 5], b[N + 5], cnt;
 bool vis[N + 5];
 bool check(int k, int c[]) {
-    if (k == 0) return c[1] % 2;
-    
+    if (k == 0) return 0;
+    int p = 0;
+    for (int i = 1; i <= k; i ++)
+        p += c[i] * (c[i] - 1) / 2;
+    int gcd = c[1];
+    for (int i = 1; i <= k; i ++)
+        gcd = __gcd(gcd, c[i]);
+    return -p % gcd == 0;
 }
 void dfs(int id) {
     if (id > n) {
@@ -15,6 +21,7 @@ void dfs(int id) {
             if (vis[i])
                 b[++ cur] = a[i];
         cnt += check(cur, b);
+        cnt %= mod;
         return ;
     }
     vis[id] = 0;
@@ -23,12 +30,10 @@ void dfs(int id) {
     dfs(id + 1);
 }
 signed main() {
-    // cin >> n;
-    // for (int i = 1; i <= n; i ++)
-    //     cin >> a[i];
-    // dfs(1);
-    // cout << cnt;
-    a[1] = 2, a[2] = 4;
-    cout << check(2, a);
+    cin >> n;
+    for (int i = 1; i <= n; i ++)
+        cin >> a[i];
+    dfs(1);
+    cout << cnt;
     return 0;
 }
