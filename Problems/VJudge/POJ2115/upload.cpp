938273f51428d870be76c8bb664893b5c5557088
ShenJiayy <2696194778@qq.com>
2026-04-26 22:26:07 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .vscode/c_cpp_properties.json
    	new file:   Problems/VJudge/.etc/desktop.ini
    	new file:   Problems/VJudge/POJ2115/data.ans
    	new file:   Problems/VJudge/POJ2115/data.in
    	new file:   Problems/VJudge/POJ2115/problem.md
    	new file:   Problems/VJudge/POJ2115/test
    	new file:   Problems/VJudge/POJ2115/upload.cpp

---
+++ Problems/VJudge/POJ2115/upload.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int unsigned long long
+// pair<int, pair<int, int>> exgcd(int a, int b) {
+//     if (b == 0) return {a, {1, 0}};
+//     auto res = exgcd(b, a % b);
+//     int gcd = res.first;
+//     int x = res.second.first, y = res.second.second;
+//     return {gcd, {y, x - a / b * y}};
+// }
+int exgcd(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1, y = 0;
+        return a;
+    }
+    int x1, y1; 
+    int gcd = exgcd(b, a % b, x1, y1);
+    x = y1, y = x1 - a / b * y1;
+    return gcd;
+}
+int solve(int a, int b, int c, int k) {
+    // It seems to solve a problem that
+    // a + xc + y(2^k) = b
+    // xc + y(2^k) = b-a
+    // to get the x_min
+    // looks like to use a `exgcd`
+    int x1, y1;
+    int gcd = exgcd(c, 1 << k, x1, y1);
+    int cc = b - a;
+    if (cc % gcd) return -1;
+    int lcm = c / gcd * (1 << k);
+    
+}
+signed main() {
+	while (1) {
+        int a, b, c, k;
+        cin >> a >> b >> c >> k;
+        if (a == 0 && b == 0 && c == 0 && k == 0)
+            break;
+        int ret = solve(a, b, c, k);
+        if (ret == -1)
+            cout << "FOREVER\n";
+        else cout << ret << endl;
+    }
+    return 0;
+}
