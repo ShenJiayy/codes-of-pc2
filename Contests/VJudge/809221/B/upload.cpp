9d0a18b6d84db545795e680f55b1782aea6b19bb
ShenJiayy <2696194778@qq.com>
2026-05-01 01:56:44 +0000

    pushup

---
+++ Contests/VJudge/809221/B/upload.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int N = 1e5;
+int a[N + 5], b[N + 5], c[N + 5];
+signed main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i ++)
+		cin >> a[i] >> b[i] >> c[i];
+	
+	return 0;
+}
---
+++ Contests/VJudge/809221/C/upload.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int N = 1e9, K = 6;
+int p[K + 5];
+signed main() {
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= k; i ++)
+		cin >> p[i];
+	
+	return 0;
+}
