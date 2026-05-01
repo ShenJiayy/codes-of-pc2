81f89b6683ed54e62b16ff6b4c1985606e9d6cd1
ShenJiayy <2696194778@qq.com>
2026-05-01 09:00:06 +0000

    123456789

---
+++ Contests/VJudge/803643/F/upload.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+	int T, n, m, p;
+	cin >> T >> n >> m >> p;
+	
+	return 0;
+}
--- Contests/VJudge/809221/C/upload.cpp
+++ Contests/VJudge/809221/C/upload.cpp
@@ -2,24 +2,38 @@
 using namespace std;
 #define int long long
 const int N = 1e9, K = 6;
-int p[K + 5];
-bool check(int x) {
+int p[K + 5], n, k;
+inline int cnt1(int x) {
 	int cnt = 0;
-	for (int i = 0; i < (1 << x); i ++) {
-		
-	}
+	for (; x; x >>= 1) cnt += x & 1;
+	return cnt;
+}
+int check(int x) {
+	int cnt = 0;
+	for (int i = 1, ppp = 1; i <= k; i ++, ppp *= -1) 
+		for (int s = 0; s < (1 << k); s ++) 
+			if (cnt1(s) == i) {
+				int lcm = 1;
+				for (int bit = 0; (1 << bit) <= s; bit ++)
+					if (s >> bit & 1)
+						lcm = lcm / __gcd(lcm, p[bit]) * p[bit];
+				if (lcm <= x)
+					cnt += x / lcm * ppp;
+			}
+	return cnt;
 }
 signed main() {
-	int n, k;
 	cin >> n >> k;
-	for (int i = 1; i <= k; i ++)
+	for (int i = 0; i < k; i ++)
 		cin >> p[i];
 	int l = 1, r = 9e18, ans;
 	while (l <= r) {
 		int mid = l + r >> 1;
-		if (check(mid))
+		if (check(mid) >= n)
 			ans = mid, r = mid - 1;
 		else l = mid + 1;
 	}
+	cout << ans;
 	return 0;
 }
+// lcm(a, b) = a / gcd(a, b) * b;
