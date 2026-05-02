2f85a3f8de00de69c0c4cd2b648e2ebf02135b3f
ShenJiayy <2696194778@qq.com>
2026-05-02 12:50:07 +0000

    update

---
+++ Contests/AtCoder/Beginner Contest 456/A/upload.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+	int x;
+	cin >> x;
+	if (3 <= x && x <= 18)
+		cout << "Yes";
+	else cout << "No";
+	return 0;
+}
