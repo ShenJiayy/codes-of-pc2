601b67d1fe753a2c240689f190622436dc559ab4
tom023 <2696194778@qq.com>
2026-08-11 11:35:53 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Contests/VJudge/834957/B/data.ans
    	modified:   Contests/VJudge/834957/B/run
    	new file:   Contests/VJudge/834957/B/scripts.sh
    	modified:   Contests/VJudge/834957/B/test
    	modified:   Contests/VJudge/834957/B/upload.cpp

--- Contests/VJudge/834957/B/upload.cpp
+++ Contests/VJudge/834957/B/upload.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 const int N = 5e5, lgN = 20;
 vector<int> g[N + 5];
+namespace bcj {
+	int f[N + 5];
+	inline void init(int n) {
+		for (int i = 1; i <= n; i ++)
+			f[i] = i;
+	}
+	int find(int id) {
+		if (f[id] != id)
+			f[id] = find(f[id]);
+		return f[id];
+	}
+	inline void merge(int x, int y) {
+		int fx = find(x), fy = find(y);
+		if (fx != fy)
+			f[fx] = fy; 
+	}
+}
 namespace tps {
 	int rd[N + 5];
 	bool vis[N + 5];
-	inline void solve(int n) {
+	inline void init(int n) {
 		memset(vis, 0, sizeof vis);
 		memset(rd, 0, sizeof rd);
 		for (int u = 1; u <= n; u ++)
@@ -26,79 +43,61 @@ namespace tps {
 			}
 		}
 	}
-	int pos[N + 5], cl;
-	inline void inpos(int n) {
-		int st = 0;
-		cl = 1;
-		for (int i = 1; i <= n; i ++)
-			if (!vis[i]) {
-				st = i;
-				break;
-			}
-		int idx = g[st][0], curr = 1;
-		pos[curr] = st;
-		while (1) {
-			if (idx == st) break;
-			cl ++;
-			pos[++ curr] = idx;
-			int nxt = g[idx][0];
-			idx = nxt;
-		}
-	}
-	inline void init(int n) {
-		solve(n);
-		inpos(n);
-	}
-}
-namespace bcj {
-	int f[N + 5];
-	inline void init(int n) {
-		for (int i = 1; i <= n; i ++)
-			f[i] = i;
-	}
-	int find(int id) {
-		if (f[id] != id)
-			f[id] = find(f[id]);
-		return f[id];
-	}
-	inline void merge(int x, int y) {
-		int fx = find(x), fy = find(y);
-		if (fx != fy)
-			f[fx] = fy; 
-	}
 }
 namespace pre {
-	int rt[N + 5], dep[N + 5], ft[N + 5];
+	int rt[N + 5], dep[N + 5], f[N + 5];
 	void dfs(int id, int ft, int fr) {
 		if (rt[id])
 			return ;
 		rt[id] = fr;
+		f[id] = ft;
+		if (f[id] == 0) f[id] = id;
 		dep[id] = dep[ft] + 1;
 		for (int nxt : g[id])
 			if (tps::vis[nxt])
 				dfs(nxt, id, fr);
 	}
-	inline void solve(int n) {
+	inline void init(int n) {
 		memset(rt, 0, sizeof rt);
 		dep[0] = -1;
 		for (int i = 1; i <= n; i ++)
-			if (!rt[i])
+			if (!tps::vis[i])
 				dfs(i, 0, i);
 		dep[0] = 0;
 	}
+}
+namespace pos {
+	int pos[N + 5], cl[N + 5], where[N + 5];
+	int cc;
 	inline void init(int n) {
-		for (int i = 1; i <= n; i ++)
-			for (int j : g[i])
-				ft[j] = i;
-		solve(n);
+		memset(pos, -1, sizeof pos);
+		memset(where, -1, sizeof where);
+		cc = 0;
+		for (int i = 1; i <= n; i ++) 
+			if (tps::vis[i] == 0 && pos[i] == -1) {
+				int st = i, curr = i, idx = 1;
+				pos[curr] = idx;
+				where[curr] = ++ cc;
+				curr = g[curr][0];
+				cl[cc] = 1;
+				while (1) {
+					if (curr == st)
+						break;
+					idx ++;
+					pos[curr] = idx;
+					where[curr] = cc;
+					cl[cc] ++;
+					curr = g[curr][0];
+				}
+			}
 	}
 }
 namespace lca {
 	int dp[N + 5][lgN + 5];
 	inline void init(int n) {
+		memset(dp, 0, sizeof dp);
 		for (int i = 1; i <= n; i ++)
-			if (tps::vis[i])
-				dp[i][0] = pre::ft[i];
+			dp[i][0] = pre::f[i];
 		for (int j = 1; j <= lgN; j ++)
 			for (int i = 1; i <= n; i ++)
 				if (tps::vis[i])
@@ -106,9 +105,9 @@ namespace lca {
 	}
 	inline int query(int x, int y) {
 		while (pre::dep[x] > pre::dep[y])
-			x = pre::ft[x];
+			x = pre::f[x];
 		while (pre::dep[y] > pre::dep[x])
-			y = pre::ft[y];
+			y = pre::f[y];
 		for (int k = lgN; k >= 0; k --)
 			if (dp[x][k] != dp[y][k])
 				x = dp[x][k], y = dp[y][k];
@@ -124,8 +123,9 @@ pair<int, int> solve(int n, int x, int y) {
 		return {dep[x] - dep[lc], dep[y] - dep[lc]};
 	}
 	int rx = pre::rt[x], ry = pre::rt[y];
-	int p1x = pre::dep[x] + (tps::pos[rx] - tps::pos[ry] + tps::cl) % tps::cl, p1y = pre::dep[y];
-	int p2x = pre::dep[x], p2y = (tps::pos[ry] - tps::pos[rx] + tps::cl) % tps::cl + pre::dep[y];
+	int l = pos::cl[pos::where[rx]];
+	int p1x = pre::dep[x] + (pos::pos[ry] - pos::pos[rx] + l) % l, p1y = pre::dep[y];
+	int p2x = pre::dep[x], p2y = (pos::pos[rx] - pos::pos[ry] + l) % l + pre::dep[y];
 	if (max(p1x, p1y) < max(p2x, p2y))
 		return {p1x, p1y};
 	if (max(p1x, p1y) > max(p2x, p2y))
@@ -144,14 +144,15 @@ int main() {
 	int n, T;
 	cin >> n >> T;
 	bcj::init(n);
-	for (int u = 1; u <= n; u ++) {
-		int v;
-		cin >> v;
+	for (int v = 1; v <= n; v ++) {
+		int u;
+		cin >> u;
 		g[u].push_back(v);
 		bcj::merge(v, u);
 	}
 	tps::init(n);
 	pre::init(n);
+	pos::init(n);
 	lca::init(n);
 	while (T --) {
 		int x, y;
