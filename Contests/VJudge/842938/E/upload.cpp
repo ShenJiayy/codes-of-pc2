d155910161bae8e2fadbf1f00f79efff318e1f8f
tom023 <2696194778@qq.com>
2026-08-21 10:08:09 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Contests/VJudge/842938/E/data.in
    	new file:   Contests/VJudge/842938/E/run
    	modified:   Contests/VJudge/842938/E/upload.cpp

--- Contests/VJudge/842938/E/upload.cpp
+++ Contests/VJudge/842938/E/upload.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N = 20;
-int dp[N + 5][N + 5], a[N + 5];
+const int N = 20, M = 2000;
+int a[N + 5];
+bool dp[M + 5];
 signed main() {
     int n, m;
     cin >> n >> m;
     for (int i = 1; i <= n; i ++)
         cin >> a[i];
+    int ans = 0;
     for (int stat = 0; stat < (1 << n); stat ++)
         if (__builtin_popcount(stat) == n - m) {
             memset(dp, 0, sizeof dp);
+            dp[0] = 1;
+            int sum = 0;
             for (int i = 1; i <= n; i ++)
-                if (stat << i - 1 & 1)
-                    for (int j = 1)
+                if ((stat >> i - 1) & 1) {
+                    sum += a[i];
+                    for (int j = sum; j >= a[i]; j --)
+                        dp[j] |= dp[j - a[i]];
+                }
+            int res = 0;
+            for (int i = 1; i <= sum; i ++)
+                res += dp[i];
+            ans = max(ans, res);
         }
+    cout << ans;
     return 0;
 }
